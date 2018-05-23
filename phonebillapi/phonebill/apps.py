a8075585b1f4f1cb27ce41ff47b198fa9654b7a1
Ricardo Dani <ricardodani@gmail.com>
2018-05-23 15:45:51 -0300

    Removed app phonebill

--- phonebillapi/phonebill/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PhonebillConfig(AppConfig):
-    name = 'phonebill'
--- phonebillapi/phonebill/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from phonebill import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('api/add-record/', views.CallRecordCreateView.as_view(), name="add-record"),
-    path('api/get-bill/', views.BillRetrieveView.as_view(), name="get-bill"),
-]
