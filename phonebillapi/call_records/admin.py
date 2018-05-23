77937ac0b317a382b46f35de3884b70f6bfa239a
Ricardo Dani <ricardodani@gmail.com>
2018-05-22 22:03:12 -0300

    Add admin to call_recorsd

--- phonebillapi/call_records/admin.py
+++ phonebillapi/call_records/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from call_records.models import Call, NotCompletedCall, CompletedCall
 
-# Register your models here.
+
+class CallAdmin(admin.ModelAdmin):
+    list_display = (
+        'call_id', 'started_at', 'ended_at', 'source', 'destination', 'price',
+        'is_completed', 'created_at', 'updated_at'
+    )
+
+
+class NotCompletedCallAdmin(CallAdmin):
+    pass
+
+
+class CompletedCallAdmin(CallAdmin):
+    pass
+
+
+admin.site.register(Call, CallAdmin)
+admin.site.register(NotCompletedCall, NotCompletedCallAdmin)
+admin.site.register(CompletedCall, CompletedCallAdmin)
