2e7de16d382712972f40caa94634186e0ae4adb9
Ricardo Dani <ricardodani@gmail.com>
2018-06-19 12:15:38 -0300

    Revert "Added procfile and requirements"
    This reverts commit b38c652ca94e534f7fd0ca1099cbd096a6f24cd3.

--- phonebillapi/phonebillapi/settings.py
+++ phonebillapi/phonebillapi/settings.py
@@ -123,8 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/srv/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/srv/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
